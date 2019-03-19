7c48b38d0325d4ffbf2906ac7422ea231ac309ac
Kristian Koivisto-Kokko <pinkkis@gmail.com>
2019-03-19 21:38:05 +0000

    moved parent in config

--- src/config/gameConfig.ts
+++ src/config/gameConfig.ts
@@ -1,8 +1,8 @@
 // phaser game config
 export const gameConfig: GameConfig = {
 	type: Phaser.AUTO,
-	parent: 'game-container',
 	scale: {
+		parent: 'game-container',
 		mode: Phaser.Scale.FIT,
 		autoCenter: Phaser.Scale.CENTER_BOTH,
 		width: 1280,
