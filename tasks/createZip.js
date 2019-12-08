bf452045e6640527caa0c3f5fb33255f5de6d984
Kristian Koivisto-Kokko <pinkkis@gmail.com>
2019-12-08 16:23:10 +0000

    convert zip task to es modules

--- tasks/createZip.js
+++ tasks/createZip.js
@@ -1,36 +1,34 @@
 // require modules
-const fs = require('fs');
-const archiver = require('archiver');
-
-const {version, buildDir, distDir} = require('../buildConfig');
+import { existsSync, createWriteStream, mkdirSync } from 'fs';
+import { basename } from 'path';
+import archiver from 'archiver';
+import { version, buildDir, distDir } from '../buildConfig';
 
 // check that build exists
-console.log(buildDir);
-console.log(distDir);
-if (!fs.existsSync(distDir)) {
+if (!existsSync(distDir)) {
 	throw new Error('production build does not exist. run `npm run build` first.');
 }
 
 // create a file to stream archive data to.
-if (!fs.existsSync(buildDir)) {
-	fs.mkdirSync(buildDir);
+if (!existsSync(buildDir)) {
+	mkdirSync(buildDir);
 }
 
-const output = fs.createWriteStream(`${buildDir}/release-${version}.zip`);
+const output = createWriteStream(`${buildDir}/release-${version}.zip`);
 const archive = archiver('zip', { zlib: { level: 9 } });
 
 archive
-	.on('warning', function (err) {
+	.on('warning', (err) => {
 		if (err.code === 'ENOENT') {
-			console.warn(err);
+			console.warn('Archive error', err);
 		} else {
 			throw err;
 		}
 	})
-	.on('error', function (err) {
-		throw err;
-	});
+	.on('error', err => { throw err; });
+
+output.on('close', () => { console.info(`Archive ${basename(output.path)}: ${archive.pointer()} bytes written.`); })
 
 archive.pipe(output);
-archive.glob('**/*', {cwd: distDir});
+archive.glob('**/*', { cwd: distDir });
 archive.finalize();
